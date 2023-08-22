f7952ece77756a890312f53b982917f2f4edaa44
saul <ruizfernandezsaul@gmail.com>
2023-08-22 16:23:58 +0100

    Adición del mapa

--- src/gui.py
+++ src/gui.py
@@ -15,46 +15,46 @@ titulo.place(relx = 0.5, rely = 0.1, anchor = "center")
 
 # Dropdown huso horario
 huso_label = Label(ventana, text = "Huso horario:")
-huso_label.place(relx = 0.15, rely = 0.25, anchor = "w")
+huso_label.place(relx = 0.05, rely = 0.25, anchor = "w")
 huso_boton_drop = IntVar()
 huso_boton_drop.set(0)
 huso_drop_menu = OptionMenu(ventana, huso_boton_drop, *husos_op)
-huso_drop_menu.place(relx = 0.25, rely = 0.25, anchor = "w")
+huso_drop_menu.place(relx = 0.15, rely = 0.25, anchor = "w")
 
 # Entry latitud
 lat_label = Label(ventana, text = "Latitud (\u00b1xx.xxº):")
-lat_label.place(relx = 0.15, rely = 0.35, anchor = "w")
+lat_label.place(relx = 0.05, rely = 0.35, anchor = "w")
 entrada_lat = Entry(ventana)
-entrada_lat.place(relx = 0.25, rely = 0.35, anchor = "w")
+entrada_lat.place(relx = 0.15, rely = 0.35, anchor = "w")
 
 # Entry longitud
 long_label = Label(ventana, text = "Longitud (\u00b1xxx.xxº):")
-long_label.place(relx = 0.15, rely = 0.45, anchor = "w")
+long_label.place(relx = 0.05, rely = 0.45, anchor = "w")
 entrada_lon = Entry(ventana)
-entrada_lon.place(relx = 0.25, rely = 0.45, anchor = "w")
+entrada_lon.place(relx = 0.15, rely = 0.45, anchor = "w")
 
 # Entry altura gnomon
 alt_label = Label(ventana, text = "Altura gnomon (m):")
-alt_label.place(relx = 0.15, rely = 0.55, anchor = "w")
+alt_label.place(relx = 0.05, rely = 0.55, anchor = "w")
 entrada_alt = Entry(ventana)
-entrada_alt.place(relx = 0.25, rely = 0.55, anchor = "w")
+entrada_alt.place(relx = 0.15, rely = 0.55, anchor = "w")
 
 # Campo de texto para anunciar un error en la entrada de datos
 error_label = Label(ventana, text = "Alguno/os de los datos introducidos no son correctos...", fg = "red")
 
 # Dropdown para seleccionar el tipo de reloj
 tipo_label = Label(ventana, text = "Tipo de reloj: ")
-tipo_label.place(relx = 0.15, rely = 0.65, anchor = "w")
+tipo_label.place(relx = 0.05, rely = 0.65, anchor = "w")
 tipo_boton_drop = StringVar()
 tipo_boton_drop.set("Horizontal")
 tipo_drop_menu = OptionMenu(ventana, tipo_boton_drop, *tipo_op)
-tipo_drop_menu.place(relx = 0.25, rely = 0.65, anchor = "w")
+tipo_drop_menu.place(relx = 0.15, rely = 0.65, anchor = "w")
 
 # Entry rotación con respecto al norte
 rot_label = Label(ventana, text = "Rotación N(xx.xº)")
-rot_label.place(relx = 0.15, rely = 0.75, anchor = "w")
+rot_label.place(relx = 0.05, rely = 0.75, anchor = "w")
 entrada_rot = Entry(ventana)
-entrada_rot.place(relx = 0.25, rely = 0.75, anchor = "w")
+entrada_rot.place(relx = 0.15, rely = 0.75, anchor = "w")
 
 # Botón "Calcular reloj"
 boton_calcular = Button(ventana, text = "Calcular Reloj", command=lambda: CalcularReloj(huso_boton_drop, entrada_lat,
@@ -62,7 +62,7 @@ boton_calcular = Button(ventana, text = "Calcular Reloj", command=lambda: Calcul
                                                                                         error_label, tipo_boton_drop,
                                                                                         entrada_rot),
                         background = "blue", foreground = "white")
-boton_calcular.place(relx = 0.25, rely = 0.85, anchor = "center")
+boton_calcular.place(relx = 0.15, rely = 0.85, anchor = "center")
 
 # Salida de texto para indicar las instrucciones de uso del programa
 texto_instrucciones_numeros = '''
@@ -123,20 +123,27 @@ gráfica, o en su defecto, consulta las medidas directamente en el
 archivo dir_programa/build/datos_reloj.csv (medidas en metros)
 '''
 instrucciones_numeros = Label(ventana, text = texto_instrucciones_numeros, justify = LEFT)
-instrucciones_numeros.place(relx = 0.40, rely = 0.18, anchor = "nw")
+instrucciones_numeros.place(relx = 0.30, rely = 0.18, anchor = "nw")
 instrucciones = Label(ventana, text = texto_instrucciones, justify = LEFT)
-instrucciones.place(relx = 0.42, rely = 0.18, anchor = "nw")
+instrucciones.place(relx = 0.32, rely = 0.18, anchor = "nw")
 
 # Pongo números a las opciones del menú de la gráfica
 numeros_opciones = Label(ventana, text = "(1)                    (2)    (3)   (4)    (5)", foreground = 'red',justify = LEFT)
-numeros_opciones.place(relx = 0.40, rely = 0.9, anchor = 'nw')
+numeros_opciones.place(relx = 0.30, rely = 0.9, anchor = 'nw')
 
 # Cargo la imagen de las opciones de la gráfica
-frame = Frame(ventana, width = 200, height = 50)
-frame.pack()
-frame.place(anchor = "nw", relx = 0.40, rely = 0.93)
+frame_opciones = Frame(ventana, width = 200, height = 50)
+frame_opciones.pack()
+frame_opciones.place(anchor = "nw", relx = 0.30, rely = 0.93)
 imagen_opciones = PhotoImage(file = "./opciones.png")
-label_opciones = Label(frame, image = imagen_opciones)
+label_opciones = Label(frame_opciones, image = imagen_opciones)
 label_opciones.pack()
+
+# Importo el mapa de google maps para seleccionar la localización y el huso horario
+mapa_frame = Frame(ventana)
+mapa_frame.pack()
+mapa_frame.place(anchor = 'center', relx = 0.79, rely = 0.55)
+mapa_wid = tkintermapview.TkinterMapView(mapa_frame, width = 450, height = 450)
+mapa_wid.pack()
 
 ventana.mainloop()
--- src/guifunc.py
+++ src/guifunc.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as image
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
-from PIL import Image, ImageTk
+import tkintermapview
 
 # Función que comprueba que los datos introducidos por el usuario son correctos
 def ComprobarDatos(latitud, longitud, altura_gnomon, rot):
